0dac906f837986210aaaa9693946aa22496ede11
pshah-lab <pshah88669@gmail.com>
2025-11-22 00:53:59 +0530

    improvised 35. Search Insert Position

--- 35. Search Insert Position.js
+++ 35. Search Insert Position.js
@@ -4,11 +4,23 @@
  * @return {number}
  */
 var searchInsert = function(nums, target) {
-    for (let i = 0; i < nums.length; i++) {
-    if (nums[i] >= target) {
-      return i;
-    }
-  }
+    let left = 0;
+    let right = nums.length - 1;
 
-  return nums.length
+    while(left<= right){
+        let mid = Math.floor((left + right) / 2);
+
+        if(nums[mid] === target){
+            return mid;
+        }
+
+        else if(nums[mid] < target){
+            left = mid + 1;
+        }
+        else{
+            right = mid - 1
+        }
+    }
+
+    return left;
 };
