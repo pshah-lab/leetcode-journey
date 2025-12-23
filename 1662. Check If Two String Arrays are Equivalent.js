147f3f4b80e5f8f95da0ad9a0875fc5f912d1ebc
pshah-lab <pshah88669@gmail.com>
2025-12-23 23:41:38 +0530

    1662. Check If Two String Arrays are Equivalent  Solved

---
+++ 1662. Check If Two String Arrays are Equivalent.js
@@ -0,0 +1,37 @@
+/**
+ * @param {string[]} word1
+ * @param {string[]} word2
+ * @return {boolean}
+ */
+var arrayStringsAreEqual = function(word1, word2) {
+    let i = 0, j = 0; 
+    let p = 0, q = 0; 
+
+    while (i < word1.length && j < word2.length) {
+        if (word1[i][p] !== word2[j][q]) return false;
+
+        p++;
+        q++;
+
+        if (p === word1[i].length) {
+            i++;
+            p = 0;
+        }
+
+        if (q === word2[j].length) {
+            j++;
+            q = 0;
+        }
+    }
+
+    return i === word1.length && j === word2.length;
+};
+
+
+
+
+
+
+var arrayStringsAreEqual = function(word1, word2) {
+    return word1.join('') === word2.join('');
+};
